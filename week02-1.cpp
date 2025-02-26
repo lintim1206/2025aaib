8ee768ae97c03eb1734f3f4b9ca18d2dfd92b960
lintim1206 <13751256@me.mcu.edu.tw>
2025-02-26 11:55:47 +0800

    Add files via upload

---
+++ week02-1.cpp
@@ -0,0 +1,14 @@
+/// week02-1.cpp F020
+///把瘋狂程設的程式
+///貼到 CodeBlocks
+
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+
+	if(a%2==1) printf("odd");
+	else printf("even");
+}
---
+++ week02-2.py
@@ -0,0 +1,7 @@
+///week02-2.py
+a=int(123)
+b=int(100)
+
+print('a是',a,'b是',b)
+print('整數除法是a//b得到',a//b)
+print('餘數a%b得到',a%b)
